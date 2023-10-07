638abfe087a6a2c4eb7e7aae57f65b6b49978407
Bennett-Wendorf <bennettwendorf@gmail.com>
2023-10-07 13:25:20 -0500

    Updated pytorch model to actually be 3 layers

--- Pytorch/standard_training_example/neural_network.py
+++ Pytorch/standard_training_example/neural_network.py
@@ -6,19 +6,19 @@ INNER_LAYER_SIZE = 3
 class NeuralNetwork(nn.Module):
     '''
     A standard neural network with 3 layers.
-        Layer 1: input_size -> INNER_LAYER_SIZE
-        Layer 2: INNER_LAYER_SIZE -> INNER_LAYER_SIZE
-        Layer 3: INNER_LAYER_SIZE -> ouptut_size
+        Layer 1: input layer
+        Layer 2: hidden layer
+        Layer 3: output layer
 
     This network is sequential and uses ReLU activation functions.
+
+    Note: With Pytorch, the input layer is not explicitly defined.
     '''
     def __init__(self, input_size, output_size, inner_layer_size=INNER_LAYER_SIZE):
         super().__init__()
         self.flatten = nn.Flatten()
         self.linear_relu_sequential_model = nn.Sequential(
             nn.Linear(input_size, inner_layer_size),
-            nn.ReLU(),
-            nn.Linear(inner_layer_size, inner_layer_size),
             nn.ReLU(),
             nn.Linear(inner_layer_size, output_size)
         )
